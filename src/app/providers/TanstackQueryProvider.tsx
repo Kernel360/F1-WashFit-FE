bcfc468a689de0da8de8a41d6fb47fe75577fbc1
wookki <bwlimtony@gmail.com>
2023-12-22 15:34:20 +0900

    feat: tanstack query 보일러 플레이트 설정

---
+++ src/app/providers/TanstackQueryProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+function TanstackQueryProvider({ children }: { children: React.ReactNode }) {
+  const [client] = useState(new QueryClient());
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default TanstackQueryProvider
