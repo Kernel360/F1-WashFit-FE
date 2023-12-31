0907e99c10a19dbed9a0da2cf312e6b806d33492
wookki <bwlimtony@gmail.com>
2023-12-31 20:26:55 +0900

    chore: build error 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const path = require('path')
+
+module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
 }
-
-module.exports = nextConfig
