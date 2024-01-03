aa8819f4cf00c3719e5833b9699dad4c2a0549cc
wookki <bwlimtony@gmail.com>
2024-01-03 22:46:47 +0900

    design: 컬러 팔레트 및 body 스타일링 변경

---
+++ src/styles/colorPalette.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  primary: 'var(--primary)',
+  secondary: 'var(--secondary)',
+  red: 'var(--red)',
+  gray100: 'var(--gray-100)',
+  gray200: 'var(--gray-200) ',
+  gray300: 'var(--gray-300)',
+  gray400: 'var(--gray-400)',
+  black: 'var(--black)',
+  white: 'var(--white)',
+};
+
+export type Colors = keyof typeof colors;
