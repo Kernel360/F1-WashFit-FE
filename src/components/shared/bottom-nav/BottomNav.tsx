8323ca8a14d327d5b1f3f95eec16a2076f653a83
wookki <bwlimtony@gmail.com>
2024-01-04 18:06:57 +0900

    feat: bottom nav 컴포넌트 제작

--- src/components/shared/bottom-nav/BottomNav.tsx
+++ src/components/shared/bottom-nav/BottomNav.tsx
@@ -1,6 +1,60 @@
+'use client';
+
+import Channel from '@icons/Channel';
+import Favorite from '@icons/Favorite';
+import Home from '@icons/Home';
+import Map from '@icons/Map';
+import Profile from '@icons/Profile';
+import classNames from 'classnames/bind';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import deleteFirstWord from '@/utils/deleteFirstWord';
+import Text from '@shared/text/Text';
+
+import styles from './BottomNav.module.scss';
+
+const cx = classNames.bind(styles);
+
 function BottomNav() {
+  const pathName = usePathname();
+  const filteredPathName = deleteFirstWord(pathName);
+
   return (
-    <div />
+    <nav className={cx('container')}>
+      <ul>
+        <li>
+          <Link href="/map">
+            <Map color={filteredPathName.startsWith('map') ? 'primary' : 'black'} />
+            <Text typography="t7" fontWeight="200" color={filteredPathName.startsWith('map') ? 'primary' : 'black'}>지도</Text>
+          </Link>
+        </li>
+        <li>
+          <Link href="/favorite">
+            <Favorite color={filteredPathName.startsWith('favorite') ? 'primary' : 'black'} />
+            <Text typography="t7" fontWeight="200" color={filteredPathName.startsWith('favorite') ? 'primary' : 'black'}>즐겨찾기</Text>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <Home color={filteredPathName === '' ? 'primary' : 'black'} />
+            <Text typography="t7" fontWeight="200" color={filteredPathName === '' ? 'primary' : 'black'}>홈</Text>
+          </Link>
+        </li>
+        <li>
+          <Link href="/channel">
+            <Channel color={filteredPathName.startsWith('channel') ? 'primary' : 'black'} />
+            <Text typography="t7" fontWeight="200" color={filteredPathName.startsWith('channel') ? 'primary' : 'black'}>채널</Text>
+          </Link>
+        </li>
+        <li>
+          <Link href="/profile">
+            <Profile color={filteredPathName.startsWith('profile') ? 'primary' : 'black'} />
+            <Text typography="t7" fontWeight="200" color={filteredPathName.startsWith('profile') ? 'primary' : 'black'}>프로필</Text>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
