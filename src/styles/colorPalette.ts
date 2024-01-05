769bf102e35a9922555b86daab26f4d60b15158d
seoye0ng <bsy0313@gmail.com>
2024-01-05 13:54:19 +0900

    design: 폰트 크기 수정 및 색상 추가

--- src/styles/colorPalette.ts
+++ src/styles/colorPalette.ts
@@ -2,12 +2,14 @@ export const colors = {
   primary: 'var(--primary)',
   secondary: 'var(--secondary)',
   red: 'var(--red)',
+  gray: 'var(--gray)',
   gray100: 'var(--gray-100)',
   gray200: 'var(--gray-200) ',
   gray300: 'var(--gray-300)',
   gray400: 'var(--gray-400)',
   black: 'var(--black)',
   white: 'var(--white)',
+  pink: 'var(--pink)',
 };
 
 export type Colors = keyof typeof colors;
