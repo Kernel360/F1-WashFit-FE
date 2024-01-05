2153db913c0cd269e7d2e3b57e86a79972615cf9
wookki <bwlimtony@gmail.com>
2024-01-05 22:00:58 +0900

    feat: TextField 컴포넌트 제작 #37

--- src/components/shared/Input/Input.tsx
+++ src/components/shared/Input/Input.tsx
@@ -1,8 +1,8 @@
-import { forwardRef } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 import './Input.module.scss';
 
 // eslint-disable-next-line prefer-arrow-callback
-const Input = forwardRef<HTMLInputElement>(function Input(props, ref) {
+const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>((props, ref) => {
   return <input {...props} ref={ref} />;
 });
 
