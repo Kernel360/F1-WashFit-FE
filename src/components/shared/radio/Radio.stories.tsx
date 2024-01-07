a79e8e0d6a13728de9fd7d541e2dcb0974b95975
wookki <bwlimtony@gmail.com>
2024-01-08 00:31:44 +0900

    test: Radio 스토리북 제작

---
+++ src/components/shared/radio/Radio.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Radio from './Radio';
+
+const meta = {
+  title: 'Shared/Radio',
+  component: Radio,
+  parameters: {
+  },
+  tags: ['autodocs'],
+  argTypes: {
+  },
+} satisfies Meta<typeof Radio>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const gender: Story = {
+  args: {
+    type: 'gender',
+    label: '남성',
+    value: 'male',
+  },
+};
+
+export const ageGroup: Story = {
+  args: {
+    type: 'ageGroup',
+    label: '10대',
+    value: '10',
+  },
+};
