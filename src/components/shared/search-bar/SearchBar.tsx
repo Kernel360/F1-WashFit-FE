b35cbd7181cb6fe717e67f51151aad88381c8ee8
seoye0ng <bsy0313@gmail.com>
2024-01-07 22:44:41 +0900

    fix: SearchBar 전체삭제 버튼 렌더링 수정 #49

--- src/components/shared/search-bar/SearchBar.tsx
+++ src/components/shared/search-bar/SearchBar.tsx
@@ -13,7 +13,7 @@ import styles from './SearchBar.module.scss';
 const cx = classNames.bind(styles);
 
 function SearchBar({ isMainPage = true }: { isMainPage?: boolean }) {
-  const [search, setSearch] = useState<string>('');
+  const [keyword, setKeyword] = useState<string>('');
 
   const handleSearch = () => {
     // console.log('검색완료');
@@ -31,12 +31,14 @@ function SearchBar({ isMainPage = true }: { isMainPage?: boolean }) {
         name="q"
         placeholder="검색어를 입력하세요."
         onKeyUp={(e) => { return e.key === 'Enter' && handleSearch(); }}
-        onChange={(e) => { return setSearch(e.target.value); }}
-        value={search}
+        onChange={(e) => { return setKeyword(e.target.value); }}
+        value={keyword}
       />
-      <button aria-label="전체 삭제 버튼" onClick={() => { return setSearch(''); }}>
+      {keyword.length > 0 && (
+      <button aria-label="전체 삭제 버튼" onClick={() => { return setKeyword(''); }}>
         <Delete />
       </button>
+      )}
       <button aria-label="필터 버튼" onClick={handleFilterDrawer}>
         <Filter />
       </button>
