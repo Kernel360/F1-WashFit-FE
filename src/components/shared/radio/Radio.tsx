f6e145eb28524d7757954994094e230cc42acc22
wookki <bwlimtony@gmail.com>
2024-01-08 00:31:10 +0900

    feat: Radio ui 제작

---
+++ src/components/shared/radio/Radio.tsx
@@ -0,0 +1,26 @@
+import { InputHTMLAttributes, forwardRef } from 'react';
+
+import classNames from 'classnames/bind';
+
+import styles from './Radio.module.scss';
+
+interface RadioProps extends InputHTMLAttributes<HTMLInputElement> {
+  type: 'gender' | 'ageGroup'
+  label: string
+  value: string | number
+}
+
+const cx = classNames.bind(styles);
+
+const Radio = forwardRef<HTMLInputElement, RadioProps>(({
+  type, label, value,
+}, ref) => {
+  return (
+    <>
+      <input id={label} type="radio" ref={ref} value={value} />
+      <label className={cx('input', { gender: type === 'gender', ageGroup: type === 'ageGroup' })} htmlFor={label}>{label}</label>
+    </>
+  );
+});
+
+export default Radio;
