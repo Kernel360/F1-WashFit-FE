b0f8cf6aff7dcd55db8aeaf7950354a7bc7c9c60
seoye0ng <bsy0313@gmail.com>
2024-01-07 15:14:37 +0900

    test: ChannelArticle 컴포넌트 스토리북 제작 #47

---
+++ src/components/shared/channel-article/ChannelArticle.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ChannelArticle from './ChannelArticle';
+
+const meta = {
+  title: 'Shared/ChannelArticle',
+  component: ChannelArticle,
+  tags: ['autodocs'],
+  argTypes: {
+    data: {
+      control: 'object',
+    },
+  },
+} satisfies Meta<typeof ChannelArticle>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const YoutubeVideo: Story = {
+  args: {
+    data: {
+      id: 1,
+      link: '/',
+      src: 'https://www.youtube.com/embed/TLPnEZFN3Z8?si=yGhuR7gYK8vZSJWv',
+      alt: '유튜브 영상',
+      profile: '/assets/profile.JPG',
+      title: 'abcdefghijklmnopqrs',
+      description: 'abcdefghijklmnopqrs',
+    },
+  },
+};
