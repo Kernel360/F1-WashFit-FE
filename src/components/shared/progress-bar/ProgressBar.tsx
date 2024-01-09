96a51b0b97bf14a7fbf3831477552fb0c6b1a0d5
seoye0ng <bsy0313@gmail.com>
2024-01-09 18:58:40 +0900

    feat: ProgressBar step click시 해당 step으로 이동 #58

--- src/components/shared/progress-bar/ProgressBar.tsx
+++ src/components/shared/progress-bar/ProgressBar.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useMemo, useState } from 'react';
+
 import classNames from 'classnames/bind';
 
 import styles from './ProgressBar.module.scss';
@@ -11,26 +13,41 @@ interface ProgressBarProps {
   currentProgress?:number;
 }
 
-function ProgressBar({ progressCount = 5, currentProgress = 3 }:ProgressBarProps) {
-  const progressCountArr = Array.from({ length: progressCount }, (_, i) => { return i + 1; });
+function ProgressBar({ progressCount = 5, currentProgress = 1 }:ProgressBarProps) {
+  const [currentStep, setCurrentStep] = useState(currentProgress);
+
+  // progressCountArr는 currentStep가 변경되지 않는 한 재생성 X
+  const progressCountArr = useMemo(() => {
+    return Array.from({ length: progressCount }, (_, i) => { return i + 1; });
+  }, [progressCount]);
+
+  // progressBarWidth는 currentStep가 변경될 때만 재생성
+  const progressBarWidth = useMemo(() => { return `${((currentStep - 1) / (progressCount - 1)) * 100}%`; }, [currentStep, progressCount]);
+
+  const handleClick = (num: number) => {
+    setCurrentStep(num);
+    // console.log(num);
+  };
 
   return (
     <div className={cx('container')}>
       <div className={cx('progressBar')} />
-      <div className={cx('progress')} style={{ width: `${((currentProgress - 1) / (progressCount - 1)) * 100}%` }} />
-      {progressCountArr.map((num, idx) => {
-        const leftPosition = `${(idx / (progressCount - 1)) * 100}%`;
-        return (
-          <span
-            key={num}
-            className={cx('progressSteps', { active: currentProgress >= num })}
-            style={{ left: leftPosition }}
-          >
-            {num}
-          </span>
-        );
-      })}
-      <span />
+      <div className={cx('progress')} style={{ width: progressBarWidth }} />
+      <div>
+        {progressCountArr.map((num, idx) => {
+          const leftPosition = `${(idx / (progressCount - 1)) * 100}%`;
+          return (
+            <button
+              key={num}
+              className={cx('progressSteps', { active: currentStep >= num })}
+              style={{ left: leftPosition }}
+              onClick={() => { return handleClick(num); }}
+            >
+              {num}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
