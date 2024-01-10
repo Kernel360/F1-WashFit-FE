6dcb2b9ac05bdf66c391801b74884bfd9c55d5b2
wookki <bwlimtony@gmail.com>
2024-01-10 15:34:43 +0900

    chore: Radio 컴포넌트 name props 삭제

--- src/components/shared/radio/Radio.tsx
+++ src/components/shared/radio/Radio.tsx
@@ -8,17 +8,16 @@ interface RadioProps extends InputHTMLAttributes<HTMLInputElement> {
   type: 'gender' | 'ageGroup' | 'additionalInfo'
   label: string
   value: string | number
-  name: string
 }
 
 const cx = classNames.bind(styles);
 
 const Radio = forwardRef<HTMLInputElement, RadioProps>(({
-  type, label, value, name, ...props
+  type, label, value, ...props
 }, ref) => {
   return (
     <>
-      <input className={cx('input')} id={label} type="radio" ref={ref} value={value} name={name} {...props} />
+      <input className={cx('input')} id={label} type="radio" ref={ref} value={value} {...props} />
       <label className={cx('label', { [type]: true })} htmlFor={label}>
         {label}
       </label>
