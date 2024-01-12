ac77fe438f2bcfeace07d6b807f4400d52fcc927
wookki <bwlimtony@gmail.com>
2024-01-13 00:48:02 +0900

    chore: vitest 경로 추가 및 provider wrapper 제작

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom';
 
 import { beforeAll, afterEach, afterAll } from 'vitest';
 
-import { server } from './src/mocks/server';
+import { server } from '@mocks/server';
 
 beforeAll(() => { return server.listen(); });
 afterEach(() => { return server.resetHandlers(); });
