4d345127c9dfc092ecda152ba69697a6b5f240e5
seoye0ng <bsy0313@gmail.com>
2024-01-13 23:24:05 +0900

    feat: useForm 추가 #73

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+
 'use client';
+
+import { useForm } from 'react-hook-form';
 
 import classNames from 'classnames/bind';
 import Link from 'next/link';
@@ -8,20 +12,27 @@ import Header from '@components/shared/header/Header';
 import Spacing from '@components/shared/spacing/Spacing';
 import TextField from '@components/shared/text-field/TextField';
 import Title from '@components/shared/title/Title';
+import { ISignUp } from '@remote/api/types/auth';
 
 import styles from './page.module.scss';
 
 const cx = classNames.bind(styles);
 
 function LoginPage() {
+  const { register, handleSubmit } = useForm<ISignUp>();
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  const onSubmit = async () => {
+    // console.log(data);
+  };
   return (
     <main>
       <Header displayLogo={false} />
       <Spacing size={30} />
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Title title="로그인" size={20} />
-        <TextField label="아이디" required placeholder="아이디" />
-        <TextField label="비밀번호" required placeholder="비밀번호" />
+        <TextField label="아이디" required placeholder="아이디" {...register('id')} />
+        <TextField label="비밀번호" required placeholder="비밀번호" {...register('password')} />
         <Spacing size={30} />
         <Button type="submit" size="medium" full>
           로그인
