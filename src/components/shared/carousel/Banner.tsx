e7e8ec9fc1e857a98b65bbd4909cb563080a6332
wookki <bwlimtony@gmail.com>
2024-01-16 00:54:14 +0900

    feat: Carousel 컴포넌트 분리

--- src/components/shared/carousel/Banner.tsx
+++ src/components/shared/carousel/Banner.tsx
@@ -6,39 +6,30 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/scss';
 import 'swiper/scss/autoplay';
 import 'swiper/scss/pagination';
-import { Idata } from './types/Carousel';
+import { IBannerdata } from './types/carousel.type';
 
-interface CarouselProps {
-  data: Idata[]
-  slidesPerView?: number
-  spaceBetween?: number
-  aspectRatio: number
-}
-
-function Carousel({
-  data, slidesPerView = 1, spaceBetween = 50, aspectRatio,
-}: CarouselProps) {
+function Banner({ bannerData }: { bannerData: IBannerdata[] }) {
   return (
     <Swiper
       modules={[Autoplay, Pagination]}
-      spaceBetween={spaceBetween}
-      slidesPerView={slidesPerView}
-      pagination={{ clickable: true }}
+      spaceBetween={20}
+      slidesPerView={1}
+      pagination={{ clickable: true, type: 'bullets' }}
+      slidesOffsetBefore={30}
+      slidesOffsetAfter={20}
       autoplay={{ delay: 2000 }}
       loop
       speed={2000}
     >
-      {data.map((slide) => {
+      {bannerData.map((slide) => {
         return (
           <SwiperSlide key={slide.id}>
             <Link href={slide.link}>
               <Image
                 src={slide.src}
                 alt={slide.alt}
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: '100%', height: 'auto', aspectRatio }}
+                width={315}
+                height={140}
               />
             </Link>
           </SwiperSlide>
@@ -48,4 +39,4 @@ function Carousel({
   );
 }
 
-export default Carousel;
+export default Banner;
---
+++ src/components/shared/carousel/RecommandList.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/scss';
+import 'swiper/scss/autoplay';
+import 'swiper/scss/pagination';
+import Flex from '@shared/flex/Flex';
+import Text from '@shared/text/Text';
+
+import { IRecommandList } from './types/carousel.type';
+
+function RecommandList({ recommandListData }: { recommandListData: IRecommandList[] }) {
+  return (
+    <Swiper
+      modules={[Autoplay]}
+      slidesPerView={4}
+      autoplay={{ delay: 2000 }}
+      loop
+      speed={2000}
+    >
+      {recommandListData.map((slide) => {
+        return (
+          <SwiperSlide key={slide.id}>
+            <Flex justify="center" align="center" direction="column">
+              <Link href={slide.link}>
+                <Image
+                  src={slide.src}
+                  alt={slide.alt}
+                  width={85}
+                  height={85}
+                />
+              </Link>
+              <Text typography="t7">{slide.productName}</Text>
+            </Flex>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+}
+
+export default RecommandList;
