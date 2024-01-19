47123087d750f1c00437031f2b6b167ee6d2390d
wookki <bwlimtony@gmail.com>
2024-01-19 19:41:15 +0900

    feat: ColorPicker 컴포넌트 제작

---
+++ src/components/details/color-picker/ColorPicker.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { InputHTMLAttributes, forwardRef } from 'react';
+
+import classNames from 'classnames/bind';
+
+import Flex from '@shared/flex/Flex';
+import Spacing from '@shared/spacing/Spacing';
+import Text from '@shared/text/Text';
+
+import styles from './ColorPicker.module.scss';
+
+interface ColorPickerProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string
+  value: string
+  color: string
+}
+
+const cx = classNames.bind(styles);
+
+const ColorPicker = forwardRef<HTMLInputElement, ColorPickerProps>(({
+  label, value, color, ...props
+}, ref) => {
+  return (
+    <Flex direction="column" justify="center" align="center" className={cx('colorPickerContainer')}>
+      <input className={cx('input')} id={label} type="radio" ref={ref} value={value} {...props} />
+      <label className={cx('label')} htmlFor={label} style={{ backgroundColor: color }} />
+      <Spacing size={6} />
+      <Text typography="t7" color="gray200">{label}</Text>
+    </Flex>
+  );
+});
+
+export default ColorPicker;
