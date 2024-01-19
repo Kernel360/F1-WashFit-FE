a66c93bb3036a76bb1d81fadfabaa0c00ef72241
wookki <bwlimtony@gmail.com>
2024-01-19 19:41:32 +0900

    test: colorPicker 스토리북 작성

---
+++ src/components/details/color-picker/ColorPicker.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ColorPicker from './ColorPicker';
+
+const meta = {
+  title: 'details/ColorPicker',
+  component: ColorPicker,
+  parameters: {
+  },
+  tags: ['autodocs'],
+  argTypes: {
+  },
+} satisfies Meta<typeof ColorPicker>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const black: Story = {
+  args: {
+    label: '검은색',
+    value: '37383C',
+    color: '37383C',
+  },
+};
+
+export const green: Story = {
+  args: {
+    label: '초록',
+    value: '2F4F4F',
+    color: '2F4F4F',
+  },
+};
