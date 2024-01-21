160f9fbd495e79c00f29b53c2a6ea27b65c7945c
seoyeong <bsy0313@gmail.com>
2024-01-21 14:56:53 +0900

    feat: Header 컴포넌트 디자인 수정(filter icon 추가) #94

--- src/components/shared/header/headerItems/RightIcon.tsx
+++ src/components/shared/header/headerItems/RightIcon.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 
 import FilledHeart from '@components/icons/FilledHeart';
+import Filter from '@components/icons/Filter';
 import Heart from '@components/icons/Heart';
 import Search from '@components/icons/Search';
 import Share from '@components/icons/Share';
@@ -13,13 +14,22 @@ import { RightIconProps } from '../types/headerType';
 
 function RightIcon({
   className,
-  displayRightIconType,
+  displayRightIconType, onFilterClick,
 }: RightIconProps) {
   const [isSaved, setIsSaved] = useState(false);
 
   const handleHeartClick = () => {
     setIsSaved((prev) => { return !prev; });
   };
+
+  if (displayRightIconType === 'filter') {
+    return (
+      <li className={className}>
+        {/* 필터가 적용됐을 때만 필터 아이콘이 바뀜 */}
+        <Filter onClick={onFilterClick} />
+      </li>
+    );
+  }
 
   if (displayRightIconType === 'search') {
     return (
--- src/components/shared/header/types/headerType.ts
+++ src/components/shared/header/types/headerType.ts
@@ -6,12 +6,13 @@ interface LeftIconProps {
 interface RightIconProps {
   className?: string;
   displayRightIconType?:string;
-
+  onFilterClick?: () => void;
 }
 
 interface HeaderProps extends LeftIconProps {
   isTransparent?: boolean;
   displayRightIconType?:string;
+  onFilterClick?: () => void;
 }
 
 export type { HeaderProps, LeftIconProps, RightIconProps };
