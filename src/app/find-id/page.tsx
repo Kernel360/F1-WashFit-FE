194b64060a61b1aeb6067f9dde671c788d142d7b
seoyeong <bsy0313@gmail.com>
2024-01-22 23:29:18 +0900

    feat: 아이디 찾기 쿼리훅 작성 #113

--- src/app/find-id/page.tsx
+++ src/app/find-id/page.tsx
@@ -7,6 +7,7 @@ import { useForm } from 'react-hook-form';
 
 import VALIDATION_MESSAGE_MAP from '@/constants/validationMessage';
 import { IFindId } from '@/remote/api/types/auth';
+import useFindId from '@/remote/queries/auth/useFindId';
 import FixedBottomButton from '@shared/fixedBottomButton/FixedBottomButton';
 import Header from '@shared/header/Header';
 import Spacing from '@shared/spacing/Spacing';
@@ -17,9 +18,21 @@ function FindUsernamePage() {
   const { register, handleSubmit, formState: { isValid, errors, isDirty } } = useForm<IFindId>({
     mode: 'onBlur',
   });
+  const { mutate } = useFindId();
 
   const onSubmit = (data: IFindId) => {
-
+    const { email } = data;
+    mutate({ email }, {
+      onError: (error) => {
+        console.error('Error:', error);
+        alert('다시 입력해주세요');
+        // TODO: 아이디 찾기 실패 시 알림 메세지 바로 출력
+      },
+      onSuccess: () => {
+        alert('회원님의 이메일로 아이디 전송완료');
+        // TODO: 아이디 전송완료 페이지 로드하기
+      },
+    });
   };
 
   return (
