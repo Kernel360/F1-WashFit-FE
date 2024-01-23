879b6c9537b589175c651f8c50950c084e851561
seoyeong <bsy0313@gmail.com>
2024-01-23 18:27:05 +0900

    feat: 스크롤 감지하는 useScroll 훅 기능 구현 #117

---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+const useScroll = (): boolean => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 1);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return isScrolled;
+};
+
+export default useScroll;
