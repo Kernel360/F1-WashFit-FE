afc95f8bc02d72b0f6535ad8987dbc6debb32a07
wookki <bwlimtony@gmail.com>
2024-01-24 00:06:44 +0900

    feat: ScrollToTop 컴포넌트 제작

---
+++ src/components/shared/scroll-to-top/ScrollToTop.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
+'use client';
+
+import classNames from 'classnames/bind';
+
+import Expand from '@/components/icons/Expand';
+import useScrollToTop from '@hooks/useScrollToTop';
+
+import styles from './ScrollToTop.module.scss';
+
+const cx = classNames.bind(styles);
+
+function ScrollToTop() {
+  const { showButton, scrollToTop } = useScrollToTop();
+
+  return showButton && (
+    <div className={cx('wrapper')}>
+      <button onClick={scrollToTop} className={cx('scroll')}>
+        <Expand width={18} height={10} isRotate color="tertiary" />
+      </button>
+    </div>
+  );
+}
+
+export default ScrollToTop;
