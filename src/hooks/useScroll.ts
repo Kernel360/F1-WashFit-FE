02667726390cb775718c0f36d9535d6ed9792d44
seoyeong <bsy0313@gmail.com>
2024-01-25 11:55:48 +0900

    refactor:  useScroll 훅에throttle을 적용해 성능 개선 #117

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,12 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
+
+import throttle from 'lodash/throttle';
 
 const useScroll = (): boolean => {
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 1);
-    };
+    const handleScroll = throttle(() => {
+      setIsScrolled(() => {
+        return window.scrollY > 1;
+      });
+    }, 300);
 
     window.addEventListener('scroll', handleScroll);
 
