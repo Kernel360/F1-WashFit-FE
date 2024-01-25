dfaa0467e501d6abfe14b0d2dffb8f24ff7c1162
seoyeong <bsy0313@gmail.com>
2024-01-25 16:57:37 +0900

    design: 비밀번호 변경 페이지, 아이디 찾기 페이지 padding 적용 #128

--- src/app/change-password/page.tsx
+++ src/app/change-password/page.tsx
@@ -5,6 +5,7 @@
 
 import { useForm } from 'react-hook-form';
 
+import classNames from 'classnames/bind';
 import dynamic from 'next/dynamic';
 
 import VALIDATION_MESSAGE_MAP from '@constants/validationMessage';
@@ -13,6 +14,10 @@ import Header from '@shared/header/Header';
 import Spacing from '@shared/spacing/Spacing';
 import TextField from '@shared/text-field/TextField';
 import Title from '@shared/title/Title';
+
+import styles from './page.module.scss';
+
+const cx = classNames.bind(styles);
 
 const FixedBottomButton = dynamic(() => { return import('@shared/fixedBottomButton/FixedBottomButton'); }, {
   ssr: false,
@@ -39,7 +44,7 @@ function ChangePasswordPage() {
     <>
       <Header isDisplayLogo={false} />
       <Spacing size={16} />
-      <main>
+      <main className={cx('mainContainer')}>
         <Title title="비밀번호 변경" description="사용하실 새 비밀번호를 입력해주세요." size={4} descriptionColor="tertiary" />
         <Spacing size={40} />
         <TextField
