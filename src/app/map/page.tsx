9cf503891be94a140f1d134050d6aaab4ca7f723
seoyeong <bsy0313@gmail.com>
2024-01-31 13:12:57 +0900

    feat: Map 제작 #141

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,8 +1,19 @@
-import BottomNav from '@shared/bottom-nav/BottomNav';
+import classNames from 'classnames/bind';
 
+import BottomNav from '@shared/bottom-nav/BottomNav';
+import KakaoMap from '@shared/map/KakaoMap';
+import SearchBar from '@shared/search-bar/SearchBar';
+
+import styles from './page.module.scss';
+
+const cx = classNames.bind(styles);
 function MapPage() {
   return (
-    <div>
+    <div className={cx('layout')}>
+      <div className={cx('searchWrapper')}>
+        <SearchBar isShadow />
+      </div>
+      <KakaoMap />
       <BottomNav />
     </div>
   );
