ba25214198152b784ebc1fc188eaba384d12d3e0
seoyeong <bsy0313@gmail.com>
2024-01-31 23:03:26 +0900

    feat: 로그인 성공시 사용자 id를 저장 #147

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import userReducer from './slices/userSlice';
+
 export const makeStore = () => {
   return configureStore({
     reducer: {
+      user: userReducer,
     },
     middleware: (getDefaultMiddleware) => {
       return getDefaultMiddleware({
