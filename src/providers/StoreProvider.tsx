6a680e01f02fda719c26691883b5261787c159a7
seoyeong <bsy0313@gmail.com>
2024-02-02 01:37:28 +0900

    feat: PersistGate 추가 #147

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -3,20 +3,26 @@
 import { useRef } from 'react';
 import { Provider } from 'react-redux';
 
-import { makeStore, AppStore } from '@stores/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-function StoreProvider({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import { makeStore, AppStore, persistor } from '@stores/store';
+
+function StoreProvider({ children }: { children: React.ReactNode }) {
   const storeRef = useRef<AppStore>();
+
   if (!storeRef.current) {
-    // Create the store instance the first time this renders
+  // Create the store instance the first time this renders
     storeRef.current = makeStore();
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return (
+    <Provider store={storeRef.current}>
+      {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default StoreProvider;
