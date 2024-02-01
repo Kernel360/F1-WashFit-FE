57445a9417d448ee6c7cff6c5b28d01f338ed78d
seoyeong <bsy0313@gmail.com>
2024-02-02 01:35:01 +0900

    feat: redux-persist가 적용된 리듀서 구현 및 persistStore 추가 #147

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,19 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './slices/userSlice';
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      user: userReducer,
-    },
-    middleware: (getDefaultMiddleware) => {
-      return getDefaultMiddleware({
-        serializableCheck: false,
-      });
-    },
-  });
+const reducers = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+  disableRehydrate: true,
 };
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
+
+export const makeStore = () => { return store; };
+
+export const persistor = persistStore(store);
 
 // Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>;
