460eccca728aeae40ab396d73969c836eb02d747
seoyeong <bsy0313@gmail.com>
2024-02-02 19:51:44 +0900

    feat: 로그인 성공시 메인페이지로 이동 #150

--- src/remote/queries/auth/useLogin.ts
+++ src/remote/queries/auth/useLogin.ts
@@ -1,12 +1,14 @@
 import { useCookies } from 'react-cookie';
 
 import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
 import { login } from '@remote/api/requests/auth/auth.post.api';
 import { useAppDispatch } from '@stores/hooks';
 import { setUserId } from '@stores/slices/userSlice';
 
 function useLogin() {
+  const router = useRouter();
   const dispatch = useAppDispatch();
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [cookies, setCookie] = useCookies(['token']);
@@ -17,6 +19,7 @@ function useLogin() {
       const { id, jwtToken } = data.value;
       setCookie('token', jwtToken, { path: '/', maxAge: 60 * 15 });
       dispatch(setUserId(id));
+      router.push('/');
     },
   });
 }
