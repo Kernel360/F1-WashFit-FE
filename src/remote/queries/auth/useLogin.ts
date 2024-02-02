3ac70616689f6bac1f4c15fe15ed742a248e7994
seoyeong <bsy0313@gmail.com>
2024-02-02 19:58:35 +0900

    refactor: useLogin 쿼리훅 success 함수 분리 #150

--- src/remote/queries/auth/useLogin.ts
+++ src/remote/queries/auth/useLogin.ts
@@ -4,6 +4,7 @@ import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
 import { login } from '@remote/api/requests/auth/auth.post.api';
+import { UserInfoType } from '@remote/api/types/auth';
 import { useAppDispatch } from '@stores/hooks';
 import { setUserId } from '@stores/slices/userSlice';
 
@@ -13,14 +14,18 @@ function useLogin() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [cookies, setCookie] = useCookies(['token']);
 
+  const onSuccess = (data : UserInfoType) => {
+    const { id, jwtToken } = data.value;
+    const cookieOptions = { path: '/', maxAge: 60 * 15 };
+
+    setCookie('token', jwtToken, cookieOptions);
+    dispatch(setUserId(id));
+    router.push('/');
+  };
+
   return useMutation({
     mutationFn: login,
-    onSuccess: (data) => {
-      const { id, jwtToken } = data.value;
-      setCookie('token', jwtToken, { path: '/', maxAge: 60 * 15 });
-      dispatch(setUserId(id));
-      router.push('/');
-    },
+    onSuccess,
   });
 }
 
