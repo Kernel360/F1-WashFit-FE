fbfe82bb6c02b185251de32393b3a2b82d930229
bottlewook <bwlimtony@gmail.com>
2024-02-02 17:10:39 +0900

    feat: dropdownMap 제작

---
+++ src/constants/dropdownMap.ts
@@ -0,0 +1,15 @@
+export const GENDER_MAP = {
+  man: '남성',
+  woman: '여성',
+} as const;
+
+export const AGE_MAP = {
+  AGE_20: '20대 이하',
+  AGE_30: '30대',
+  AGE_40: '40대',
+  AGE_50: '50대',
+  AGE_60: '60대 이상',
+} as const;
+
+export type GenderType = keyof typeof GENDER_MAP;
+export type AgeType = keyof typeof AGE_MAP;
