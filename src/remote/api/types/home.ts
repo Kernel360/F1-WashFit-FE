cd10e5d700a422d5b327de66923b7f79873a229b
bottlewook <bwlimtony@gmail.com>
2024-02-03 15:47:12 +0900

    feat: IProduct, IProductDetails 타입 추가 및 수정

--- src/remote/api/types/home.ts
+++ src/remote/api/types/home.ts
@@ -12,14 +12,34 @@ export interface IRecommendProducts extends IBanner {
 }
 
 export interface IProduct {
-  id: number
-  img: string
-  name: string
+  productNo: number
+  productName: string
+  barcode: string
+  imageSource: string
+  reportNumber: string
+  safetyStatus: string
+  viewCount: number
+  createdAt: string
+  createdBy: string
+  modifiedAt: string
+  modifiedBy: string
   brand: string
-  category: string
-  warningLevel: string
+  upperItem: string
+}
+
+export interface IProductDetails extends IProduct {
+  companyName: string
+  productType: string
+  manufactureType: string
+  manufactureMethod: string
+  weight: string
+  mainSubstance: string
+  grade: number
+  reviewCnt: number
+  viewCount: number
 }
 
 export type BannerType = ICommon<IBanner[]>;
 export type RecommendProductsType = ICommon<IRecommendProducts[]>;
 export type ProductType = ICommon<IProduct[]>;
+export type ProductDetailsType = ICommon<IProductDetails[]>;
