9018b84b8dc4aa75ec34fcc28e7f69c8bcda8330
bottlewook <bwlimtony@gmail.com>
2024-02-03 21:10:12 +0900

    feat: 제품 상세 페이지 msw 기능 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,9 @@
 import { authHandlers } from './authHandler';
 import { homeHandlers } from './homeHandler';
+import { productHandlers } from './productHandler';
 
 export const handlers = [
   ...authHandlers,
   ...homeHandlers,
+  ...productHandlers,
 ];
