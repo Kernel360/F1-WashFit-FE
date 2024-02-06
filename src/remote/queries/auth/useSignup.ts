0e01e3c5b99d6fe8632c4a376bed88c72c601f66
seoyeong <bsy0313@gmail.com>
2024-02-06 17:23:54 +0900

    feat: 회원가입 성공시 로그인 페이지로 이동 #159

--- src/remote/queries/auth/useSignup.ts
+++ src/remote/queries/auth/useSignup.ts
@@ -1,9 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
 import { signup } from '@remote/api/requests/auth/auth.post.api';
 
 function useSignup() {
-  return useMutation({ mutationFn: signup });
+  const router = useRouter();
+
+  const onSuccess = () => {
+    router.push('/login');
+  };
+
+  return useMutation({ mutationFn: signup, onSuccess });
 }
 
 export default useSignup;
