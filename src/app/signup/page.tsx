add3628c43443f706b5b930f53f4d95cd036b89d
seoyeong <bsy0313@gmail.com>
2024-02-07 14:20:11 +0900

    feat: 회원가입 페이지 약관 동의하기 클릭시에 이용약관이 보여지도록 구현 #159

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,6 +2,7 @@
 
 'use client';
 
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import {
@@ -15,6 +16,7 @@ import Button from '@shared/button/Button';
 import DropdownField from '@shared/dropdown-field/DropdownField';
 import Header from '@shared/header/Header';
 import Spacing from '@shared/spacing/Spacing';
+import Terms from '@shared/terms/Terms';
 import TextField from '@shared/text-field/TextField';
 import Title from '@shared/title/Title';
 
@@ -47,90 +49,104 @@ function SignupPage() {
     });
   };
 
+  const [step, setStep] = useState(1);
+
+  const onNext = () => {
+    setStep((currentStep) => { return currentStep + 1; });
+  };
+
   // TODO: 아이디 textfield onBlur 시 중복된 아이디 검사
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Header />
-      <Spacing size={20} />
-      <main className="mainContainer">
-        <Title title="회원가입" />
-        <TextField
-          label="아이디"
-          required
-          placeholder="아이디"
-          {...register('id', {
-            required: true,
-            pattern: VALIDATION_MESSAGE_MAP.id.value,
-          })}
-          hasError={!!errors.id}
-          helpMessage={VALIDATION_MESSAGE_MAP.id.message}
-        />
-        <TextField
-          label="비밀번호"
-          required
-          placeholder="비밀번호"
-          isPasswordType
-          {...register('password', {
-            required: true,
-            pattern: VALIDATION_MESSAGE_MAP.password.value,
-          })}
-          hasError={!!errors.password}
-          helpMessage={VALIDATION_MESSAGE_MAP.password.message}
-        />
-        <TextField
-          label="비밀번호 확인"
-          required
-          placeholder="비밀번호 확인"
-          isPasswordType
-          {...register('confirmPassword', {
-            required: true,
-            validate: (confirmPassword: string) => {
-              if (watch('password') !== confirmPassword) {
-                return false;
-              }
-              return true;
-            },
-          })}
-          hasError={!!errors.confirmPassword}
-          helpMessage={VALIDATION_MESSAGE_MAP.confirmPassword.message}
-        />
-        <TextField
-          label="이메일"
-          required
-          placeholder="이메일"
-          {...register('email', {
-            required: true,
-            pattern: VALIDATION_MESSAGE_MAP.email.value,
-          })}
-          hasError={!!errors.email}
-          helpMessage={VALIDATION_MESSAGE_MAP.email.message}
-        />
-        <DropdownField
-          label="성별"
-          required
-          selectedLabel={GENDER_MAP[watch('gender') as GenderType]}
-          type="profile"
-          options={GENDER_OPTIONS}
-          {...register('gender', {
-            required: true,
-          })}
-        />
-        <Spacing size={12} />
-        <DropdownField
-          required
-          label="연령대"
-          selectedLabel={AGE_MAP[watch('age') as AgeType]}
-          type="profile"
-          options={AGE_OPTIONS}
-          {...register('age', {
-            required: true,
-          })}
-        />
-        <Spacing size={120} />
-        <Button type="submit" disabled={!isValid || !isDirty} size="medium" full>약관 동의하러 가기</Button>
-      </main>
-      <Spacing size={42.5} />
+      {step === 1 && (
+        <>
+          <Header />
+          <Spacing size={20} />
+          <main className="mainContainer">
+            <Title title="회원가입" />
+            <TextField
+              label="아이디"
+              required
+              placeholder="아이디"
+              {...register('id', {
+                required: true,
+                pattern: VALIDATION_MESSAGE_MAP.id.value,
+              })}
+              hasError={!!errors.id}
+              helpMessage={VALIDATION_MESSAGE_MAP.id.message}
+            />
+            <TextField
+              label="비밀번호"
+              required
+              placeholder="비밀번호"
+              isPasswordType
+              {...register('password', {
+                required: true,
+                pattern: VALIDATION_MESSAGE_MAP.password.value,
+              })}
+              hasError={!!errors.password}
+              helpMessage={VALIDATION_MESSAGE_MAP.password.message}
+            />
+            <TextField
+              label="비밀번호 확인"
+              required
+              placeholder="비밀번호 확인"
+              isPasswordType
+              {...register('confirmPassword', {
+                required: true,
+                validate: (confirmPassword: string) => {
+                  if (watch('password') !== confirmPassword) {
+                    return false;
+                  }
+                  return true;
+                },
+              })}
+              hasError={!!errors.confirmPassword}
+              helpMessage={VALIDATION_MESSAGE_MAP.confirmPassword.message}
+            />
+            <TextField
+              label="이메일"
+              required
+              placeholder="이메일"
+              {...register('email', {
+                required: true,
+                pattern: VALIDATION_MESSAGE_MAP.email.value,
+              })}
+              hasError={!!errors.email}
+              helpMessage={VALIDATION_MESSAGE_MAP.email.message}
+            />
+            <DropdownField
+              label="성별"
+              required
+              selectedLabel={GENDER_MAP[watch('gender') as GenderType]}
+              type="profile"
+              options={GENDER_OPTIONS}
+              {...register('gender', {
+                required: true,
+              })}
+            />
+            <Spacing size={12} />
+            <DropdownField
+              required
+              label="연령대"
+              selectedLabel={AGE_MAP[watch('age') as AgeType]}
+              type="profile"
+              options={AGE_OPTIONS}
+              {...register('age', {
+                required: true,
+              })}
+            />
+            <Spacing size={120} />
+            <Button disabled={!isValid || !isDirty} onClick={onNext} size="medium" full>약관 동의하러 가기</Button>
+          </main>
+          <Spacing size={42.5} />
+        </>
+      )}
+
+      {step === 2 && (
+      <Terms type="signup" onClick={() => { return onSubmit(watch()); }} />
+      )}
     </form>
   );
 }
