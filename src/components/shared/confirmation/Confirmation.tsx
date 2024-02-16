4f450d3a5b7c3456adb57e4805c37ebef343f667
seoyeong <bsy0313@gmail.com>
2024-02-16 17:37:37 +0900

    feat: Confirmation 컴포넌트 제작 #165

---
+++ src/components/shared/confirmation/Confirmation.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { CompleteType } from '@constants/completeMessage';
+import Button from '@shared/button/Button';
+import Flex from '@shared/flex/Flex';
+import Spacing from '@shared/spacing/Spacing';
+import Text from '@shared/text/Text';
+
+import './Confirmation.scss';
+
+interface ConfirmationProps extends CompleteType {}
+
+function Confirmation({ options, title, description }:ConfirmationProps) {
+  const router = useRouter();
+
+  return (
+    <div className="confirmationContainer">
+      <div className="titleWrapper">
+        <Text typography="t1" bold display="block" textAlign="start">
+          {title}
+        </Text>
+        <Spacing size={8} />
+        <Text display="block" typography="t6" color="gray500" textAlign="start">
+          {description}
+        </Text>
+      </div>
+      <Flex direction="column" gap={12} className="buttonWrapper">
+        {Object.entries(options).map(([key, value]) => {
+          return (
+            <Button key={key} size="large" full onClick={() => { return router.push(`/${key}`); }}>
+              {value}
+            </Button>
+          );
+        })}
+      </Flex>
+    </div>
+  );
+}
+export default Confirmation;
