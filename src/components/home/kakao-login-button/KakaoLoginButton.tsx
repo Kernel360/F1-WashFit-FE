df1e423e201c9653d212487c3e9e4511c55f9fc3
bottlewook <bwlimtony@gmail.com>
2024-02-18 23:07:29 +0900

    feat: 카카오 로그인 인가코드 로직 제작

--- src/components/home/kakao-login-button/KakaoLoginButton.tsx
+++ src/components/home/kakao-login-button/KakaoLoginButton.tsx
@@ -3,13 +3,14 @@
 import classNames from 'classnames/bind';
 import Image from 'next/image';
 
-import KakaoScript from '@shared/kakao-script/KakaoScript';
+import KakaoLoginScript from '@shared/kakao-script/KakaoLoginScript';
 
 import styles from './KakaoLoginButton.module.scss';
 
 const cx = classNames.bind(styles);
 
 function KakaoLoginButton() {
+  // TODO: 인가코드를 이용해서 백엔드에 api 요청하기
   const handleLoginKakao = () => {
     window.Kakao.Auth.authorize({
       redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
@@ -21,7 +22,7 @@ function KakaoLoginButton() {
       <button className={cx('kakaoLoginWrapper')} type="button" onClick={handleLoginKakao}>
         <Image src="/assets/kakao_login_medium_wide.png" alt="카카오 로그인 버튼" width={300} height={45} />
       </button>
-      <KakaoScript />
+      <KakaoLoginScript />
     </>
   );
 }
