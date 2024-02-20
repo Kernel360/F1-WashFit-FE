b509f4ba5633b3a3361b534404c3f0d24dc56a17
bottlewook <bwlimtony@gmail.com>
2024-02-21 00:49:40 +0900

    feat: kakaoscript 컴포넌트 제작

--- src/components/home/kakao-login-button/KakaoLoginButton.tsx
+++ src/components/home/kakao-login-button/KakaoLoginButton.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import Image from 'next/image';
 
-import KakaoLoginScript from '@shared/kakao-script/KakaoLoginScript';
+import KakaoScript from '@shared/kakao-script/KakaoScript';
 
 function KakaoLoginButton() {
   // TODO: 인가코드를 이용해서 백엔드에 api 요청하기
@@ -17,7 +17,7 @@ function KakaoLoginButton() {
       <button type="button" onClick={handleLoginKakao}>
         <Image src="/assets/kakaoLoginButton.png" alt="카카오 로그인 버튼" width={40} height={40} />
       </button>
-      <KakaoLoginScript />
+      <KakaoScript />
     </>
   );
 }
