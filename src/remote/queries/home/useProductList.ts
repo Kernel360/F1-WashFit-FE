53dac778358fff7df2e280839bbbd50c640bae32
bottlewook <bwlimtony@gmail.com>
2024-02-20 11:40:56 +0900

    feat: useProdcutList 쿼리훅 제작

--- src/remote/queries/home/useProductList.ts
+++ src/remote/queries/home/useProductList.ts
@@ -1,10 +1,38 @@
-import { useQuery } from '@tanstack/react-query';
+import { useCallback } from 'react';
+
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 import { getProductList } from '@remote/api/requests/home/home.get.api';
-import { ProductType } from '@remote/api/types/home';
 
 function useProductList() {
-  return useQuery<ProductType>({ queryKey: ['productList'], queryFn: getProductList });
+  const {
+    data: productList, isLoading, fetchNextPage, isFetching, hasNextPage = false,
+  } = useInfiniteQuery({
+    queryKey: ['productList'],
+    queryFn: ({ pageParam }) => { return getProductList(pageParam as number, 10); },
+    getNextPageParam: (lastPage) => {
+      return (
+        lastPage.value.last
+          ? undefined
+          : lastPage.value.pageable.pageNumber + 1
+      );
+    },
+    suspense: true,
+  });
+
+  const loadMore = useCallback(async () => {
+    if (hasNextPage === false || isFetching) {
+      return;
+    }
+
+    await fetchNextPage();
+  }, [fetchNextPage, hasNextPage, isFetching]);
+
+  const data = productList?.pages.map((page) => { return page.value.content; }).flat();
+
+  return {
+    data, isLoading, loadMore, isFetching, hasNextPage,
+  };
 }
 
 export default useProductList;
