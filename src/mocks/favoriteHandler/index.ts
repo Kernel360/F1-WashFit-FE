0b10b2edfbad716e68b416c41d4e795e1b0d916c
seoyeong <bsy0313@gmail.com>
2024-02-22 20:44:17 +0900

    test: 즐겨찾기 제품 목록 MSW 작성 및 mock data 정의 #204

---
+++ src/mocks/favoriteHandler/index.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { http, HttpResponse } from 'msw';
+
+import { MOCK_FAVORITE_DATA } from './mock';
+
+export const favoriteHandlers = [
+  /* ----- like 제품 목록 정보 api ----- */
+  http.get(`${process.env.NEXT_PUBLIC_BASE_URL}/likes`, () => {
+    return HttpResponse.json(MOCK_FAVORITE_DATA);
+  }),
+];
