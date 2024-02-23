a2ca50440e7316fb709edadc2afb9ffb86a27b8b
seoyeong <bsy0313@gmail.com>
2024-02-23 19:16:23 +0900

    feat: 즐겨찾기 제품 목록 get 요청, 쿼리훅에서 정려타입 삭제 #204

--- src/mocks/favoriteHandler/index.ts
+++ src/mocks/favoriteHandler/index.ts
@@ -5,7 +5,7 @@ import { MOCK_FAVORITE_DATA } from './mock';
 
 export const favoriteHandlers = [
   /* ----- like 제품 목록 정보 api ----- */
-  http.get(`${process.env.NEXT_PUBLIC_BASE_URL}/likes`, () => {
+  http.get(`${process.env.NEXT_PUBLIC_BASE_URL}/likes?*`, () => {
     return HttpResponse.json(MOCK_FAVORITE_DATA);
   }),
 ];
--- src/remote/api/requests/favorite/favorite.get.api.ts
+++ src/remote/api/requests/favorite/favorite.get.api.ts
@@ -2,8 +2,8 @@ import { ProductListInfoType } from '../../types/home';
 import { getRequest } from '../requests.api';
 
 /* ----- like 제품 목록 api ----- */
-export const getFavoriteList = async () => {
-  const response = await getRequest<ProductListInfoType>('/likes');
+export const getFavoriteList = async (pageNum: number, size: number) => {
+  const response = await getRequest<ProductListInfoType>(`/likes?&page=${pageNum}&size=${size}`);
 
   return response;
 };
