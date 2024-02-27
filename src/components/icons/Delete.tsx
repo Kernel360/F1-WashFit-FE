dad1e38f5fbf2d972e7545b63d83dca574ad38ff
bottlewook <bwlimtony@gmail.com>
2024-02-27 18:52:42 +0900

    feat: SearchBar 컴포넌트 수정

--- src/components/icons/Delete.tsx
+++ src/components/icons/Delete.tsx
@@ -6,7 +6,7 @@ interface DeleteProps {
   fillColor?: Colors
 }
 
-function Delete({ size = 17, color = 'white', fillColor = 'gray100' }: DeleteProps) {
+function Delete({ size = 17, color = 'white', fillColor = 'black' }: DeleteProps) {
   return (
     <svg width={size} height={size} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M7.99998 14.6666C11.6819 14.6666 14.6666 11.6818 14.6666 7.99992C14.6666 4.31802 11.6819 1.33325 7.99998 1.33325C4.31808 1.33325 1.33331 4.31802 1.33331 7.99992C1.33331 11.6818 4.31808 14.6666 7.99998 14.6666Z" fill={colors[fillColor]} />
