94b868424fd3b18f7d5261665e459168190e7744
seoyeong <bsy0313@gmail.com>
2024-02-27 23:38:37 +0900

    feat: ChannelArticle 컴포넌트에서 video를 YouTube 컴포넌트로 변경 #213

--- src/components/shared/channel-article/ChannelArticle.tsx
+++ src/components/shared/channel-article/ChannelArticle.tsx
@@ -1,32 +1,35 @@
-import classNames from 'classnames/bind';
-import Image from 'next/image';
+import YouTube from 'react-youtube';
 
+import classNames from 'classnames/bind';
+// import Image from 'next/image';
+
+import { IVideo } from '@remote/api/types/channel';
 import Text from '@shared/text/Text';
 
 import styles from './ChannelArticle.module.scss';
 import ChannelMoreBtn from './ChannelMoreBtn';
-import { IVideoData } from './types/ChannelArticleType';
 
 interface ChannelArticleProps {
-  data: IVideoData;
+  data: IVideo;
 }
-/*
- * data는 백엔드에서 넘어오는 데이터를 참고해서 다시 바뀔 수 있음
-*/
+
 const cx = classNames.bind(styles);
 
 function ChannelArticle({ data }: ChannelArticleProps) {
+  const opts = {
+    width: '100%',
+    height: '100%',
+    playerVars: {
+      autoplay: 0,
+    },
+  };
   return (
     <article className={cx('article')}>
       <div className={cx('videoContainer')}>
-        <object
-          data={data.src}
-          type="text/html"
-          aria-label={data.title}
-        />
+        <YouTube className={cx('video')} videoId={data.id.videoId} opts={opts} />
       </div>
       <div className={cx('videoInfoContainer')}>
-        <div className={cx('imgBox')}>
+        {/* <div className={cx('imgBox')}>
           <Image
             src={data.profile}
             alt="유튜버 프로필 이미지"
@@ -34,13 +37,13 @@ function ChannelArticle({ data }: ChannelArticleProps) {
             height={0}
             layout="responsive"
           />
-        </div>
+        </div> */}
         <div className={cx('infoBox')}>
           <Text className="ellipsis" typography="t6" whiteSpace="nowrap">
-            {data.title}
+            {data.snippet.title}
           </Text>
           <Text className="ellipsis" typography="t7" color="gray300" whiteSpace="nowrap">
-            {data.description}
+            {data.snippet.description}
           </Text>
         </div>
         <ChannelMoreBtn />
