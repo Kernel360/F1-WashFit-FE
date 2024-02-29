aefe96bcf40a8f0e1b7f1e846da9c3f4dd6a400f
bottlewook <bwlimtony@gmail.com>
2024-02-29 19:40:23 +0900

    chore: 쿠키 수정

--- src/remote/api/instance.api.ts
+++ src/remote/api/instance.api.ts
@@ -1,4 +1,11 @@
-import axios, { Axios } from 'axios';
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-console */
+import axios, {
+  Axios, InternalAxiosRequestConfig, AxiosError, AxiosResponse,
+} from 'axios';
+
+import { getCookie } from '@utils/cookies';
+import logOnDev from '@utils/logOnDev';
 
 export const axiosInstance: Axios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -10,3 +17,79 @@ export const axiosInstance: Axios = axios.create({
   },
   timeout: 3000,
 });
+
+axiosInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+    /**
+     * request 직전 공통으로 진행할 작업
+     */
+    const token = getCookie('token') as string;
+    console.log('token', token);
+    if (config && config.headers) {
+      if (token) {
+        console.log('token 안쪽', token);
+        config.headers.Authorization = token;
+      }
+    }
+    if (process.env.NODE_ENV === 'development') {
+      const { method, url } = config;
+      // eslint-disable-next-line
+      logOnDev(`🚀 [API] ${method?.toUpperCase()} ${url} | Request`);
+    }
+    return config;
+  },
+  (error: AxiosError | Error): Promise<AxiosError> =>
+  /** * request 에러 시 작업 */
+  // eslint-disable-next-line @typescript-eslint/brace-style
+  { return Promise.reject(error); },
+);
+
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse): AxiosResponse => {
+    /**
+     * http status가 20X이고, http response가 then으로 넘어가기 직전 호출
+     */
+
+    return response;
+  },
+  (error: AxiosError | Error): Promise<AxiosError> => {
+    /**
+     * http status가 20X가 아니고, http response가 catch로 넘어가기 직전 호출
+     */
+    if (process.env.NODE_ENV === 'development') {
+      if (axios.isAxiosError(error)) {
+        const { message } = error;
+        const { method, url } = error.config as InternalAxiosRequestConfig;
+        const { status, statusText } = error.response as AxiosResponse;
+        logOnDev(
+          `🚨 [API] ${method?.toUpperCase()} ${url} | Error ${status} ${statusText} | ${message}`,
+        );
+        switch (status) {
+          case 401: {
+            // 로그인 필요 메시지 연결
+            break;
+          }
+          case 403: {
+            // 권한 필요 메시지 연결
+            break;
+          }
+          case 404: {
+            // 잘못된 요청 메시지 연결
+            break;
+          }
+          case 500: {
+            // 서버 문제 발생 메시지 연결
+            break;
+          }
+          default: {
+            // 알 수 없는 오류 발생 메시지 연결
+            break;
+          }
+        }
+      } else {
+        logOnDev(`🚨 [API] | Error ${error.message}`);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
