7156f587d313c413f83ccf5acb2b54964d12c5cf
bottlewook <bwlimtony@gmail.com>
2024-02-29 17:00:05 +0900

    fix: favorite api end point 수정

--- src/remote/api/requests/favorite/favorite.get.api.ts
+++ src/remote/api/requests/favorite/favorite.get.api.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { SearchFilterType } from '@/constants/searchByMap';
 
 import { ProductListInfoType } from '../../types/home';
@@ -7,9 +8,11 @@ import { getRequest } from '../requests.api';
 export const getFavoriteList = async (
   pageNum: number,
   size: number,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   sortType: SearchFilterType,
 ) => {
-  const response = await getRequest<ProductListInfoType>(`/likes?&sortType=${sortType}&page=${pageNum}&size=${size}`);
+  // const response = await getRequest<ProductListInfoType>(`/likes?sortType=${sortType}&page=${pageNum}&size=${size}`);
+  const response = await getRequest<ProductListInfoType>(`/likes?page=${pageNum}&size=${size}`);
 
   return response;
 };
