a95b8419e9c2b017837fd42a7ca22ddae7a38994
bottlewook <bwlimtony@gmail.com>
2024-02-29 20:21:35 +0900

    feat: 카카오 로그인 리다이렉트 및 토큰 관리 추가 #232

--- src/app/oauth/kakao/page.tsx
+++ src/app/oauth/kakao/page.tsx
@@ -3,9 +3,13 @@
 import { useCallback, useEffect } from 'react';
 
 import axios from 'axios';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import { getRequest } from '@remote/api/requests/requests.api';
+import { UserInfoType } from '@remote/api/types/auth';
+import { useAppDispatch } from '@stores/hooks';
+import { setUserId } from '@stores/slices/userSlice';
+import { setCookie } from '@utils/cookies';
 
 const GRANT_TYPE = 'authorization_code';
 
@@ -21,28 +25,38 @@ interface IKakaoToken {
 }
 
 function KakaoPage() {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
-  /* eslint-disable no-console */
-  console.log(code, '인가 코드');
 
   const loginHandler = useCallback(async () => {
-    const response = await axios.post<null, IKakaoToken>(
-      `https://kauth.kakao.com/oauth/token?grant_type=${GRANT_TYPE}&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&code=${code}&client_secret=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET}`,
-      {},
-      {
-        headers: { 'Content-type': 'application/x-www-form-urlencoded;charset=utf-8' },
-      },
-    );
-    console.log(response.data.access_token, 'access_token');
-    const accessToken = await getRequest<IKakaoToken>('/member/login/forKakao', {
-      headers: {
-        Authorization: response.data.access_token,
-      },
-    });
-    // TODO: 리다이렉트 작업
-    return accessToken;
-  }, [code]);
+    try {
+      const response = await axios.post<null, IKakaoToken>(
+        `https://kauth.kakao.com/oauth/token?grant_type=${GRANT_TYPE}&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&code=${code}&client_secret=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET}`,
+        {},
+        {
+          headers: { 'Content-type': 'application/x-www-form-urlencoded;charset=utf-8' },
+        },
+      );
+
+      const data = await getRequest<UserInfoType>('/member/login/forKakao', {
+        headers: {
+          Authorization: response.data.access_token,
+        },
+      });
+
+      const { id, jwtToken } = data.value;
+      const cookieOptions = { path: '/', maxAge: 60 * 15 };
+
+      setCookie('token', jwtToken, cookieOptions);
+      dispatch(setUserId(id));
+      router.push('/');
+    } catch (e) {
+      console.error(e);
+    }
+  }, [code, dispatch, router]);
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
