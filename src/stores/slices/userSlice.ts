5eae11a38c3f119414d1c61ced51740bcd799214
bottlewook <bwlimtony@gmail.com>
2024-03-01 14:48:05 +0900

    feat: 비밀번호 찾기 쿼리훅 제작 #236

--- src/stores/slices/userSlice.ts
+++ src/stores/slices/userSlice.ts
@@ -2,26 +2,30 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface UserState {
   id: string | null;
+  email: string | null;
 }
 
 const initialState: UserState = {
   id: null,
+  email: null,
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUserId: (state, action: PayloadAction<string | null>) => {
+    setUserId: (state, action: PayloadAction<UserState>) => {
       return {
         ...state,
-        id: action.payload,
+        id: action.payload.id,
+        email: action.payload.email,
       };
     },
     clearUserId: (state) => {
       return {
         ...state,
         id: null,
+        email: null,
       };
     },
   },
