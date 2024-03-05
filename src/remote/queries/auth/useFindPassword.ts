781491db19bec88e9ff332298ea91933b66f4b2a
bottlewook <bwlimtony@gmail.com>
2024-03-05 10:40:28 +0900

    feat: 비빌번호 찾기 토스트 추가

--- src/remote/queries/auth/useFindPassword.ts
+++ src/remote/queries/auth/useFindPassword.ts
@@ -2,16 +2,25 @@ import { toast } from 'react-toastify';
 
 import { useMutation } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
 
 import { findPassword } from '@remote/api/requests/auth/auth.post.api';
 
 function useFindPassword() {
+  const router = useRouter();
+  const onSuccess = () => {
+    toast.success('이메일이 전송되었습니다.');
+    router.push('/login');
+  };
+
   const onError = (error: AxiosError) => {
     if (error.response?.status === 400) {
       toast.error('유효하지 않은 아이디입니다.');
+    } else {
+      toast.error('서버 에러입니다');
     }
   };
-  return useMutation({ mutationFn: findPassword, onError });
+  return useMutation({ mutationFn: findPassword, onSuccess, onError });
 }
 
 export default useFindPassword;
