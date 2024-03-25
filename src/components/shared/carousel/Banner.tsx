d51eaa5b785f594520ea9262c91d52339f07b58a
eunhak <akfmzh1207@naver.com>
2024-03-25 15:45:32 +0900

    fix: banner 디자인 변경

--- src/components/shared/carousel/Banner.tsx
+++ src/components/shared/carousel/Banner.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 
 import Image from 'next/image';
@@ -8,36 +9,48 @@ import Link from 'next/link';
 import './Banner.scss';
 import { BannerType, IBanner } from '@remote/api/types/home';
 
-const settings = {
-  infinite: true,
-  speed: 2000,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  adaptiveHeight: true,
-  autoplaySpeed: 5000,
-  arrows: false,
-  centerPadding: '100px',
-};
+import Text from '../text/Text';
 
 function Banner({ bannerData }: { bannerData: BannerType }) {
   const bannerList: IBanner[] = bannerData.value;
+  const [currentSlide, setCurrentSlide] = useState(0);
 
+  const settings = {
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    adaptiveHeight: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    centerPadding: '100px',
+    beforeChange: (current: number, next: number) => { return setCurrentSlide(next); },
+  };
   return (
-    <Slider {...settings} className="banner-slide">
-      {bannerList.map((slide) => {
-        return (
-          <Link href={slide.productNo.toString()} key={slide.productNo}>
-            <Image
-              src={slide.imageSource}
-              alt={slide.alt}
-              objectFit="cover"
-              fill
-            />
-          </Link>
-        );
-      })}
-    </Slider>
+    <div style={{ position: 'relative' }}>
+      <Slider {...settings} className="banner-slide">
+        {bannerList.map((slide) => {
+          return (
+            <Link href={slide.productNo.toString()} key={slide.productNo}>
+              <Image src={slide.imageSource} alt={slide.alt} objectFit="cover" fill />
+            </Link>
+          );
+        })}
+      </Slider>
+      <div style={{ position: 'absolute', left: '92%', top: '90%' }}>
+        <Text
+          typography="t8"
+          color="white"
+          css={{ borderRadius: '20px', backgroundColor: 'black', padding: '4px 12px' }}
+        >
+          {currentSlide + 1}
+          {' '}
+          /
+          {bannerList.length}
+        </Text>
+      </div>
+    </div>
   );
 }
 
