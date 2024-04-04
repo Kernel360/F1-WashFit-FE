3bff571169b4c2eb6a9877f9f3c9a56228cddffa
eunhak <akfmzh1207@naver.com>
2024-04-04 22:36:24 +0900

    fix: SearchBar 조건문 추가

--- src/components/shared/search-bar/SearchBar.tsx
+++ src/components/shared/search-bar/SearchBar.tsx
@@ -5,6 +5,7 @@
 import { forwardRef } from 'react';
 
 import classNames from 'classnames/bind';
+import { useRouter } from 'next/navigation';
 
 import Delete from '@components/icons/Delete';
 // import Filter from '@components/icons/Filter';
@@ -15,42 +16,52 @@ import styles from './SearchBar.module.scss';
 const cx = classNames.bind(styles);
 
 interface SearchBarProps {
-  isShadow?: boolean
-  handleSearch: () => void
+  isShadow?: boolean;
+  handleSearch: () => void;
 }
 
 // eslint-disable-next-line max-len
-const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(({ isShadow = false, handleSearch }, ref) => {
-  return (
-    <form
-      className={cx('container', { shadow: isShadow })}
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSearch();
-      }}
-    >
-      <Search />
-      <input
-        type="search"
-        name="q"
-        placeholder="검색어를 입력하세요."
-        onKeyUp={(e) => { return e.key === 'Enter' && handleSearch(); }}
-        ref={ref}
-      />
-      <button
-        type="button"
-        aria-label="전체 삭제 버튼"
-        onClick={() => {
-          if (ref != null && typeof ref !== 'function') {
-            ref.current!.value = '';
+const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(
+  ({ isShadow = false, handleSearch }, ref) => {
+    const router = useRouter();
+    return (
+      <form
+        className={cx('container', { shadow: isShadow })}
+        onSubmit={(e) => {
+          e.preventDefault();
+          let keyword = '';
+          if (ref && 'current' in ref) {
+            keyword = ref.current?.value || '';
           }
+          router.push(`/search?keyword=${keyword}`);
+          handleSearch();
         }}
-        style={{ cursor: 'pointer' }}
       >
-        <Delete fillColor="gray300" />
-      </button>
-    </form>
-  );
-});
+        <Search />
+        <input
+          type="search"
+          name="q"
+          placeholder="검색어를 입력하세요."
+          onKeyUp={(e) => {
+            return e.key === 'Enter' && handleSearch();
+          }}
+          ref={ref}
+        />
+        <button
+          type="button"
+          aria-label="전체 삭제 버튼"
+          onClick={() => {
+            if (ref != null && typeof ref !== 'function') {
+              ref.current!.value = '';
+            }
+          }}
+          style={{ cursor: 'pointer' }}
+        >
+          <Delete fillColor="gray300" />
+        </button>
+      </form>
+    );
+  },
+);
 
 export default SearchBar;
