7f2d3f15b39ced146dd937de72b3992097dac01b
eunhak <akfmzh1207@naver.com>
2024-04-04 22:45:04 +0900

    feat: 카메라 ocr 기능 도입중

---
+++ src/components/shared/ocr/Ocr.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+/* eslint-disable @next/next/no-async-client-component */
+import { createWorker } from 'tesseract.js';
+
+const Ocr = async () => {
+  const worker = await createWorker('eng+kor', 1, {});
+  const Img = async () => {
+    const {
+      data: { text },
+    } = await worker.recognize(
+      'https://tesseract.projectnaptha.com/img/eng_bw.png'
+    );
+
+    // console.log(text);
+    await worker.terminate();
+    return text;
+  };
+
+  return { Img };
+};
+
+export default Ocr;
