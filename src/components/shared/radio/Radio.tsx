3bdff4f0193acce6438f961c6ba63eac534ef188
eunhak <akfmzh1207@naver.com>
2024-04-09 23:02:09 +0900

    fix:라디오 ui 수정

--- src/components/shared/radio/Radio.tsx
+++ src/components/shared/radio/Radio.tsx
@@ -1,28 +1,51 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { InputHTMLAttributes, forwardRef } from 'react';
 
 import classNames from 'classnames/bind';
 
+import Flex from '../flex/Flex';
+
 import styles from './Radio.module.scss';
 
 interface RadioProps extends InputHTMLAttributes<HTMLInputElement> {
-  type: 'gender' | 'ageGroup' | 'additionalInfo' | 'filter' | 'product'
-  label: string
-  value: string | number
+  type:
+  | 'gender'
+  | 'ageGroup'
+  | 'additionalInfo'
+  | 'filter'
+  | 'product'
+  | 'washFilter';
+  label: string;
+  value: string | number;
+  img?: JSX.Element;
 }
 
 const cx = classNames.bind(styles);
 
-const Radio = forwardRef<HTMLInputElement, RadioProps>(({
-  type, label, value, ...props
-}, ref) => {
-  return (
-    <>
-      <input className={cx('input')} id={label} type="radio" ref={ref} value={value} {...props} />
-      <label className={cx('label', { [type]: true })} htmlFor={label}>
-        {label}
-      </label>
-    </>
-  );
-});
+const Radio = forwardRef<HTMLInputElement, RadioProps>(
+  ({
+    type, label, value, img, ...props
+  }, ref) => {
+    return (
+      <>
+        <input
+          className={cx('input')}
+          id={label}
+          type="radio"
+          ref={ref}
+          value={value}
+          {...props}
+        />
+
+        <label className={cx('label', { [type]: true })} htmlFor={label}>
+          <Flex direction="column" justify="center" align="center">
+            {img}
+            {label}
+          </Flex>
+        </label>
+      </>
+    );
+  },
+);
 
 export default Radio;
