9632a5fd6003e4d171eab62383e1d913788b325c
eunhak <akfmzh1207@naver.com>
2024-04-11 18:37:20 +0900

    feat: icon 추가

---
+++ src/components/icons/WarningWrap.tsx
@@ -0,0 +1,22 @@
+function WarningWrap() {
+  return (
+    <svg
+      width="48"
+      height="45"
+      viewBox="0 0 48 45"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        clipPath: 'polygon(0% 10%, 10% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      }}
+    >
+      <path
+        id="warning_back"
+        d="M0 0H48L24.7059 21.7969L0 45V0Z"
+        fill="#F1663A"
+      />
+    </svg>
+  );
+}
+
+export default WarningWrap;
